a6b8af1da72846a0ec14a0e492f9996869378786
Akash <kolakkalakash@gmail.com>
2024-06-24 18:24:19 +0530

    basic discord bot code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# bot.py
 import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.default() 
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
