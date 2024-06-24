2a297bba690d6137c24453cd804f92129c9b0cf9
Ayush Sonar <ayushsonar@atharvacoe.ac.in>
2024-06-24 22:31:42 +0530

    integrated with main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from google.cloud import texttospeech
 import discord
 from dotenv import load_dotenv
+import ttsapi as api
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -21,7 +22,22 @@ async def on_message(message):
     if message.author == client.user or not message.guild:
         return
 
-    if message.channel.id == tts_channel_id :
-        await message.channel.send('pong')
+    if message.channel.id == tts_channel_id:
+        api.tts(message.content)
+        # Join the user's voice channel
+        if message.author.voice:
+            vc = message.author.voice.channel
+            voice_client = await vc.connect()
+
+            # Play the 'output.mp3' file
+            voice_client.play(discord.FFmpegPCMAudio('output.mp3'), after=lambda e: print('done', e))
+
+            # Wait for the audio to play before disconnecting
+            while voice_client.is_playing():
+                pass
+
+            await voice_client.disconnect()
+        else:
+            await message.channel.send("You are not in a voice channel.")
 
 client.run(TOKEN)
