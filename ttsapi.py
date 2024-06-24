5c4c1d663dc55539654ae01082b969509aee86be
Ayush Sonar <ayushsonar@atharvacoe.ac.in>
2024-06-24 22:31:42 +0530

    integrated with main.py

---
+++ ttsapi.py
@@ -0,0 +1,35 @@
+from google.cloud import texttospeech
+import os
+
+text_block= "fuck you"
+
+def tts(text_block):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'ckc-tts-bot-28b03e81ae46.json'
+
+    client = texttospeech.TextToSpeechClient()
+
+    synthesis_input = texttospeech.SynthesisInput(text=text_block)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        name="en-US-Studio-O"
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3,
+        effects_profile_id=["small-bluetooth-speaker-class-device"],
+        speaking_rate=1.0,
+        pitch=1.0
+    )
+
+    response = client.synthesize_speech(
+        input=synthesis_input, 
+        voice=voice, 
+        audio_config=audio_config
+    )
+
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print("Audio content written to file 'output.mp3'")
+
+tts(text_block)
