39d86fd6d08da4bd92796eb924379470d3820659
Ayush Sonar <ayushsonar@atharvacoe.ac.in>
2024-06-24 21:01:45 +0530

    bot replies with pong and ttsapi works

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 # bot.py
 import os
-
+from google.cloud import texttospeech
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+tts_channel_id = 1254793464269504696
 
 intents = discord.Intents.default() 
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or not message.guild:
+        return
+
+    if message.channel.id == tts_channel_id :
+        await message.channel.send('pong')
 
 client.run(TOKEN)
